c53c0af927ee912bf34bf7571f97ab8631ad23c3
mko_io <me@mko.io>
2019-09-06 15:06:09 +0000

    works

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 const through = require("through2")
-const babelCore = require("@babel-core")
+const babelCore = require("@babel/core")
 const jsxPlugin = require("@babel/plugin-transform-react-jsx") 
+const { StringDecoder } = require("string_decoder")
 
+const stringDecoder = new StringDecoder()
 module.exports = transformJsxPlugin
 
 function transformJsxPlugin(options) {
     options = options || {}
-    return through.obj(async (file, encoding, callback) {
-        const result = await transformJsx(file)
-        callback(null, result))
+    return through.obj(async function (file, encoding, next) {
+        const buffer = file.contents
+        const text = stringDecoder.end(buffer)
+        const result = await transformJsx(text)
+        file.contents = Buffer.from(result)
+        this.push(file)
+        next()
     })
 }
 
-async function transform_jsx(str) {
+async function transformJsx(str) {
     const result = await babelCore.transformAsync(str, {
         plugins: [jsxPlugin]
         })
